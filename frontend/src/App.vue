bca1c25bc96c46ed9060df67a1711d85ce05f314
michaelteddy009 <michaelteddy009@gmail.com>
2024-08-31 06:00:50 +0300

    feature/YG-16-add-global-front-end-footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,10 +2,12 @@
   <div id="app">
     <Navbar />
     <router-view />
+    <Footer />
   </div>
 </template>
 <script setup lang="ts">
 import Navbar from "@/components/global/Navbar.vue";
+import Footer from "./components/global/Footer.vue";
 
 </script>
 <style>
